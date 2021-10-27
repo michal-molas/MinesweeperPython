b030c42d2c5099cf7eeba846cd8096f88a6aa03f
Michał Molas <49158235+michal-molas@users.noreply.github.com>
2021-10-27 18:37:39 +0200

    checking arguments

--- main.py
+++ main.py
@@ -24,21 +24,31 @@ def run(width, height, num_bombs):
 
 
 if __name__ == "__main__":
-    width = int(sys.argv[1])
-    height = int(sys.argv[2])
-    num_bombs = int(sys.argv[3])
+    if len(sys.argv) > 4:
+        print('Too many arguments.')
+    elif len(sys.argv) < 4:
+        print('Not enough arguments.')
+    else:
+        correct_input = True
+        for i in range(1, 4):
+            if not sys.argv[i].isdigit() or int(sys.argv[i]) < 0:
+                print('Argument ' + sys.argv[i] + ' is not a positive integer.')
+                correct_input = False
 
-    correct_input = True
+        if correct_input:
+            width = int(sys.argv[1])
+            height = int(sys.argv[2])
+            num_bombs = int(sys.argv[3])
 
-    if width > 50 or width <= 0:
-        print('Width must be in range from 1 to 50.')
-        correct_input = False
-    if height > 25 or height <= 0:
-        print('Height must be in range from 1 to 25.')
-        correct_input = False
-    if num_bombs <= 0 or num_bombs > width * height - 1:
-        print('number of bombs must be in range from 1 to (width * height - 1)')
-        correct_input = False
-    
-    if correct_input:
-        run(width, height, num_bombs)
+            if width > 50 or width <= 0:
+                print('Width must be in range from 1 to 50.')
+                correct_input = False
+            if height > 25 or height <= 0:
+                print('Height must be in range from 1 to 25.')
+                correct_input = False
+            if num_bombs <= 0 or num_bombs > width * height - 1:
+                print('number of bombs must be in range from 1 to (width * height - 1)')
+                correct_input = False
+            
+            if correct_input:
+                run(width, height, num_bombs)
