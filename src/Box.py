0c60fb5febc6df59ad96eb52bf5a75fe07cc2c7c
Michał Molas <49158235+michal-molas@users.noreply.github.com>
2021-10-27 18:06:26 +0200

    Add files via upload

--- src/Box.py
+++ src/Box.py
@@ -2,15 +2,15 @@ import pygame
 
 
 class Box:
-    uncov_textures = [pygame.image.load("../assets/none.png"), pygame.image.load("../assets/one.png"),
-                      pygame.image.load("../assets/two.png"), pygame.image.load("../assets/three.png"),
-                      pygame.image.load("../assets/four.png"), pygame.image.load("../assets/five.png"),
-                      pygame.image.load("../assets/six.png"), pygame.image.load("../assets/seven.png"),
-                      pygame.image.load("../assets/eight.png"), pygame.image.load("../assets/bomb.png"),]
+    uncov_textures = [pygame.image.load("assets/none.png"), pygame.image.load("assets/one.png"),
+                      pygame.image.load("assets/two.png"), pygame.image.load("assets/three.png"),
+                      pygame.image.load("assets/four.png"), pygame.image.load("assets/five.png"),
+                      pygame.image.load("assets/six.png"), pygame.image.load("assets/seven.png"),
+                      pygame.image.load("assets/eight.png"), pygame.image.load("assets/bomb.png"),]
 
-    cov_textures = [pygame.image.load("../assets/empty.png"),
-                    pygame.image.load("../assets/flag.png"),
-                    pygame.image.load("../assets/question_mark.png")]
+    cov_textures = [pygame.image.load("assets/empty.png"),
+                    pygame.image.load("assets/flag.png"),
+                    pygame.image.load("assets/question_mark.png")]
 
     val_dict = {
         "0": 0,
